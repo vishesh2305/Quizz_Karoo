0fe8958e12e68ca82da6c25561a8b7c5adcdfcb6
Vishesh <visheshvasu2305@gmail.com>
2025-03-20 14:24:39 +0530

    Port Changed

--- app.py
+++ app.py
@@ -185,6 +185,8 @@ def scientific_quizzes():
 
 # --- Run the app ---
 if __name__ == "__main__":
+    port= int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
     with app.app_context():
         db.create_all()
         print("Database initialized successfully!")
